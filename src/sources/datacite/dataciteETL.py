b5f30b98cc47e55d0b9acbcd83f884fbaa434177
Gary Frewin <48474558+GaryFrewin@users.noreply.github.com>
2022-07-27 10:37:43 +0100

    Connection to DB established and api data in

--- src/sources/datacite/dataciteETL.py
+++ src/sources/datacite/dataciteETL.py
@@ -1,5 +1,11 @@
+from pymongo import MongoClient
+import time
+import requests
+import json
 
-apiquery = "ddd"
+url = 'https://api.datacite.org/dois'
+payload = {'query': 'Rothamsted AND types.resourceTypeGeneral: (Dataset OR Model OR Other OR Software)','page[size]': '1000'}
+
 transformSourceQuery = "ddd"
 loadQuery = "ddd"
 
@@ -10,10 +16,27 @@ def execute():
     load()
 
 
-
 def extract():
+
+    start_time = time.time()
     print('extract datacite data from API')
+
+    #connect to mongo
+    client = MongoClient("mongodb://superuser:AgainstMake1nsect@149.155.16.39/")
+    db = client["TestDB"]
+    collection = db['DataCiteRAW']
+    collection.delete_many({}) 
     #call api
+    response = requests.get(url, params=payload)
+    #print(response.json())
+    kpi_data = json.loads(response.text)
+
+    i = 1
+    for data in kpi_data['data']:
+        
+        print("Dataset " + str(i) + ". type = " + str(type(data)))
+        collection.insert_one(data)
+        i+=1
     #cursors
     #parse response
     #place into mongo #abstract database connection to repository layer
@@ -21,6 +44,11 @@ def extract():
 
 def transform():
     print('grab datacite from MONGO and transform')
+    client = MongoClient("mongodb://superuser:AgainstMake1nsect@149.155.16.39/")
+    db = client["TestDB"]
+    collection = db['DataCiteRAW']
+    #start_time = time.time()
+    results = collection.find()
     #read fromdatabase
     #transform
     #load into staging table 
