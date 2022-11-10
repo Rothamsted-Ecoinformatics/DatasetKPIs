3283a8943403e43633ea5ee54562b1227e94b47f
stellalberto <alberto.stella@rothamsted.ac.uk>
2022-11-10 11:10:22 +0000

    Added comparison

---
+++ src/services/comparisonservice.py
@@ -0,0 +1,30 @@
+import datetime
+
+from src.data.mongo import MongoRepository as mdb
+import json
+
+
+def compare():
+    db = mdb()
+    rawids = set(db.getrawdb()['DataCite'].find().distinct('_id'))
+    stageids = set(db.getstagingdb()['DataCite'].find().distinct('_id'))
+    diffids = rawids - stageids
+
+    # diffs = db.getrawdb()['DataCite'].find({"_id": {"$in": list(diffids)}})
+    diffs = db.getrawdb()['DataCite'].find({"$and": [
+        {"attributes.publisher": {"$ne": "The Global Biodiversity Information Facility"}},
+        {"attributes.descriptions.description": {"$ne": {"$regex": "A dataset containing"}}},
+        {"_id": {"$in": list(diffids)}}]})
+    dictionary = {'createdOn': str(datetime.datetime.now()), 'excludedDocs': list(diffs)}
+    # for doc in diffs:
+    #     type(doc)
+    #     dictionary
+
+    # print(list(diffs))
+
+    # Serializing json
+    with open("sample.json", "w+") as outfile:
+        json.dump(dictionary, outfile, default=str, indent=4)
+
+
+compare()
